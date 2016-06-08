e16e5dfe5fa652c4d7564c46344b20bab887df1f
Nishant Rodrigues <nish_10z@yahoo.com>
2016-06-08 15:15:54 -0400

    Added license header to multi-motif testing scripts

--- testing/run_all_configs_multi.sh
+++ testing/run_all_configs_multi.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# This file is part of CONCUSS, https://github.com/theoryinpractice/concuss/, and is
+# Copyright (C) North Carolina State University, 2015. It is licensed under
+# the three-clause BSD license; see LICENSE.
+#
+
+# This script runs the multi-motif pipeline against all configuration files
+# in testing/config/
+# It takes two arguments - 1. graph, 2. multi-motif file.
 
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -8,6 +17,6 @@ for f in $FILES
   do
     echo Config file ${bold} $f ${normal}:
     echo
-    ./concuss.py $1 multi $f -m testing/graphs/motifs/multifile.txt
+    ./concuss.py $1 multi $f -m $2
     echo
   done
--- testing/test_multi.sh
+++ testing/test_multi.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#
+# This file is part of CONCUSS, https://github.com/theoryinpractice/concuss/, and is
+# Copyright (C) North Carolina State University, 2015. It is licensed under
+# the three-clause BSD license; see LICENSE.
+#
+
 . $(dirname $(which $0))/test_common.sh
 
 "${path}/multi.sh" testing/graphs/karate.txt 3
